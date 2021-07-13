90d966ac12a896ff99613da09a8b421debbe4f14
Rashmika Jayasekara <87364822+RashmikaJay16@users.noreply.github.com>
2021-07-13 10:21:42 +0000

    Not yet complete

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,24 @@
 #include <stdio.h>
 
 int main() {
+  int s1, s2;
+  double avg, sum;
+
+  printf("Enter the mark (subject1) :");
+  scanf("%d", &s1);
+
+  printf("Enter the mark (subject2) :");
+  scanf("%d", &s2);
+
+  sum = s1 + s2;
+
+  printf("Total :%.2f\n", sum);
+
+  avg = sum/2;
+
+  printf("Average : %.2f", avg);
+
+
   
   return 0;
 }
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,26 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  int distance;
+  float amount;
+
+  printf("Input the distance the van has travelled :");
+  scanf("%d", &distance);
+
+  if(distance <= 30)
+  {
+    amount = 50 * distance;
+  }
+  else if(distance >= 30 )
+  {
+    amount = (30 * distance + (distance - 30) * 40);
+  }
+  else
+  {
+   printf("Error Distance\n");
+  }
+
+  printf("Amount = %.2f", amount);
   
   return 0;
 }
